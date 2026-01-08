c0b851c9ed0e153234b3bffdb3090eb362b90c7d
yoshitetsu <yoshida.tetsuhiro@grimoire.co>
2026-01-08 12:21:48 +0900

    fix

--- assets/shared.js
+++ assets/shared.js
@@ -2,6 +2,10 @@
 (function(){
   try {
     if (window.__PAGE_ID__) {
+      const lastPage = localStorage.getItem("last_page");
+      if (lastPage !== window.__PAGE_ID__) {
+        localStorage.setItem("prev_page", lastPage || "");
+      }
       localStorage.setItem("last_page", window.__PAGE_ID__);
       localStorage.setItem("last_seen_at", Date.now().toString());
     }
